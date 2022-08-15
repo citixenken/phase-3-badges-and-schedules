9cf2f68f1275bc3b1e65b6e591d609e483293be7
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-15 07:13:59 +0300

    commit tree 37b806d96624320186bad8f19cb504a1b46045bd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+#============================================
+
+def batch_badge_creator(attendees)
+    attendees.map { |attendee|  "Hello, my name is #{attendee}." }  
+end
+#============================================
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) { |attendee, room| "Hello, #{attendee}! You'll be assigned to room #{room}!"}
+end
+#============================================
+#method calling other methods
+def printer(attendees)
+    #methods below return arrays therefore...
+    batch_badge_creator(attendees).each { |created_badge| puts created_badge}
+    assign_rooms(attendees).each { |room_assignment| puts room_assignment}
+end
